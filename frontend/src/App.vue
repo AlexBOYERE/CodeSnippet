bd830488081be67866fe340168ad1d752e03cd06
AlexBOYERE <proalexandreboyere@gmail.com>
2025-02-10 07:31:12 +0100

    rename folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Navbar from './components/menu/Navbar.vue'
-import MainCSnippet from './components/MainCSnippet.vue'
 </script>
 
 <template>
@@ -9,6 +8,7 @@ import MainCSnippet from './components/MainCSnippet.vue'
   </header>
 
   <main class="pt-5">
+  <!--  Gérer la navigation en se basant sur l'URL  -->
     <router-view />
   </main>
 </template>
